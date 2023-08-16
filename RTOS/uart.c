23cdaf67bc6d31af4a581274255e483ffa2cd8b3
Craig Hackney <iratahack@digitalxfer.com>
2023-08-16 09:06:51 -0700

    Attempt interrupt driven UART output.

--- RTOS/uart.c
+++ RTOS/uart.c
@@ -5,9 +5,13 @@
 #include "FreeRTOS.h"
 #include "stream_buffer.h"
 
-#define MAX_RX_STREAM 8
-StreamBufferHandle_t rxStream;
-BaseType_t pxHigherPriorityTaskWoken;
+#define TX_ISR  0
+
+#define MAX_UART_STREAM 8
+static StreamBufferHandle_t rxStream;
+#if TX_ISR
+static StreamBufferHandle_t txStream;
+#endif
 
 /**
  * @brief Construct the UART RX ISR
@@ -16,6 +20,7 @@ BaseType_t pxHigherPriorityTaskWoken;
  */
 ISR(USART_RX_vect)
 {
+    static BaseType_t pxHigherPriorityTaskWoken;
     unsigned char data = UDR0;
 
     pxHigherPriorityTaskWoken = pdFALSE;
@@ -26,28 +31,63 @@ ISR(USART_RX_vect)
         portYIELD_FROM_ISR();
 }
 
+#if TX_ISR
+ISR(USART_UDRE_vect)
+{
+    static BaseType_t pxHigherPriorityTaskWoken;
+    unsigned char data;
+
+    pxHigherPriorityTaskWoken = pdFALSE;
+    if (xStreamBufferReceiveFromISR(txStream, &data, 1, &pxHigherPriorityTaskWoken) > 0)
+    {
+        UDR0 = data;
+    }
+    else
+    {
+        // Disable UART TX interrupts
+        UCSR0B &= ~_BV(UDRIE0);
+    }
+
+    // Exit through the scheduler if a higher priority task was scheduled
+    if (pxHigherPriorityTaskWoken)
+        portYIELD_FROM_ISR();
+}
+#endif
+
 static int uart_putchar(char c, FILE *stream)
 {
     if (c == '\n')
         uart_putchar('\r', stream);
 
+#if TX_ISR
+    xStreamBufferSend(txStream, &c, 1, portMAX_DELAY);
+    // Enable TX interrupt
+    UCSR0B |= _BV(UDRIE0);
+#else
     while (!(UCSR0A & _BV(UDRE0)))
         ; /* Wait for empty transmit buffer*/
     UDR0 = c;
-
+#endif
     return (c);
 }
 
+/**
+ * @brief Read a character from the UART
+ *
+ * @param stream
+ * @return Byte read from UART or EOF on error.
+ */
 static int uart_getchar(FILE *stream)
 {
     unsigned char c;
 
     // Read from the rxStream
-    xStreamBufferReceive(rxStream, &c, 1, portMAX_DELAY);
+    if (xStreamBufferReceive(rxStream, &c, 1, portMAX_DELAY) != 1)
+        return (EOF);
 
     // Convert CR to LF
     if (c == '\r')
-        c = '\n';
+        return ('\n');
 
     return (c);
 }
@@ -58,12 +98,20 @@ static FILE mystdin = FDEV_SETUP_STREAM(NULL, uart_getchar, _FDEV_SETUP_READ);
 
 void initUART(void)
 {
-    rxStream = xStreamBufferCreate(MAX_RX_STREAM, 1);
-    // Power on the UART
-    power_usart0_enable();
+    // Create the TX/RX stream buffers
+    // used to communicate to/from the ISR's
+    rxStream = xStreamBufferCreate(MAX_UART_STREAM, 1);
+#if TX_ISR
+    txStream = xStreamBufferCreate(MAX_UART_STREAM, 1);
+#endif
     // Setup stdio
     stdout = stderr = &mystdout;
     stdin = &mystdin;
+
+    // Power on the UART
+    power_usart0_enable();
+
+    // Set UART baud rate
 #include <util/setbaud.h>
     UBRR0H = UBRRH_VALUE;
     UBRR0L = UBRRL_VALUE;
@@ -72,7 +120,10 @@ void initUART(void)
 #else
     UCSR0A &= ~(1 << U2X0);
 #endif
-    UCSR0C = 0x06; /* Set frame format: 8data, 1stop bit  */
+
+    // Configure UART frame format N81
+    UCSR0C = 0x06;
+
     // Enable TX, RX and RX interrupts
     UCSR0B = _BV(TXEN0) | _BV(RXEN0) | _BV(RXCIE0);
 }
